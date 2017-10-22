3d2f927f05f08b76f9b9c2e59b6c6de00f92827a
Unknown <andy.chal@orange.fr>
2017-10-22 17:10:58 +0200

    main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+typedef struct{
+  int var;
+}data_t;
+
+int main()
+{
+
+  return 0;
+}
+
+int eventStart(data_t * data)
+{
+
+  return 0;
+}
+
+int eventUpdate(data_t * data)
+{
+
+  return 0;
+}
