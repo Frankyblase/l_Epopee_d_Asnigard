e6958bbcd2bc43eb6659a13da72bdd4264b02e75
Frankyblase <pascal.ruffie3@gmail.com>
2017-10-22 18:17:32 +0200

    test 	modifié :         main.c

--- main.c
+++ main.c
@@ -1,27 +1,27 @@
 #include<stdio.h>
-#include "GameEngine/gameEngine.h"   // inclus le moteur de Andy , les guillemets sont ici pour faire la diff entre les bilbotek et les fichiers sa mere
 
 typedef struct{
   int var;
 }data_t;
 
+#include "GameEngine/gameEngine.h"   // inclus le moteur de Andy , les guillemets sont ici pour faire la diff entre les bilbotek et les fichiers sa mere
+
+
 int main()
 {
-
-
-printf("ceci est");
+	data_t mydataloul;
+	mydataloul.var=42;
+	GameEngine_data gameengine;
+	gameeengine = GE_start(mydataloul, "Epopee d Asnigard",150,100,50);	//prises dans GE.h
+	GE_Update(&gameengine);
   return 0;
 }
 
-int eventStart(data_t * data)
-{
-
-  return 0;
-}
 
 int eventUpdate(data_t * data)
 {
 
+	printf("yolo\n");
   return 0;
 }
 
